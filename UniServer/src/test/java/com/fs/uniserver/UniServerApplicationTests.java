a2edbcc66f39206421e0cfe74dbf09d417f3bbf0
bikumary <bikumary@LIN60017688.corp.capgemini.com>
2022-02-15 12:53:49 +0530

    First Commit

---
+++ UniServer/src/test/java/com/fs/uniserver/UniServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.uniserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
